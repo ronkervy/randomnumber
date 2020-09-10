12f0a3544fdb544ce58641fa8141144f5992e7bf
Ron kervy Palacay <kervy00@gmail.com>
2020-09-10 18:31:04 +0800

    edited

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react';
+import './styles/style.css';
 
 function App() {
+
+  const [selected,setSelected] = useState({
+    num : 0,
+    xnum : []
+  });
+
+  const [numrange,setNumRange] = useState({
+      min : 0,
+      max : 0
+  });
+
+  useEffect(()=>{
+      console.log(numrange.min,numrange.max);
+  },[numrange]);
+
+  function randomize(){
+      setSelected({...selected,num : 0});
+      let randNum = Math.floor(Math.random() * ( numrange.max + 1 - numrange.min ) + numrange.min);      
+      
+      setTimeout(()=>{
+        if( selected.xnum.indexOf( randNum ) !== -1 ) return setSelected({...selected,num:"Number already selected."});
+        setSelected(prevState=>{
+          return {
+              num : randNum,
+              xnum : [...prevState.xnum,randNum]
+          }
+        }); 
+      },3000);
+      
+  }
+
+  const GeneratingNum = ()=>{
+      return  <h1>Generating Number</h1>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {selected.num ? <h1>{selected.num}</h1> : <GeneratingNum />}
+        <small>
+          <em>Eliminated numbers : {selected.xnum.join(',')}</em>< br/>
+        </small>
+        <input type="number" placeholder="from" name="min" onChange={e=>{setNumRange({...numrange,min : parseInt(e.currentTarget.value)})}}/>
+        <input type="number" placeholder="to" name="max" onChange={e=>{setNumRange({...numrange,max:parseInt(e.currentTarget.value)})}}/>
+        <button onClick={randomize}>Eliminate</button>
     </div>
   );
 }
