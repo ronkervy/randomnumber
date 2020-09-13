c111e449d5eac2716bd32cc1cab340a8536b4f23
Ron kervy Palacay <kervy00@gmail.com>
2020-09-14 00:22:31 +0800

    edited

--- src/App.js
+++ src/App.js
@@ -1,70 +1,69 @@
 import React,{useState,useEffect} from 'react';
+import Loader from './component/Loader';
 import './styles/style.css';
 
 function App() {
 
-  const [selected,setSelected] = useState({
-    num : 1,
-    xnum : [],
-    rnum : []
-  });
-  
-  const [numrange,setNumRange] = useState({
-      min : 0,
-      max : 0
+  const [selected,setSelected] = useState(1);
+  const [numrange,setNumrange] = useState({
+    start : 1,
+    end : 1,
+    xnum : []
   });
 
-  useEffect(()=>{
+  function handleNumber(){      
       let arr = [];
-      for( let i = numrange.min; i <= numrange.max; i++ ){
+
+      for( let i = numrange.start; i <= numrange.end; i++ ){
           arr.push(i);
-      } 
-
-      selected.rnum = arr;
-
-  },[numrange,selected]);
-
-  function randomize(){
-
-      let randNum = Math.floor(Math.random() * ( numrange.max + 1 - numrange.min ) + numrange.min);      
-
-      setSelected(prevState=>{
-          return {
-              ...prevState,
-              num : 0              
-          }
-      });
-
-      let newArr = selected.rnum.filter(x=> selected.xnum.indexOf(x) === -1 );
-
-      console.log(newArr);
-
-      setTimeout(()=>{
-        if( selected.xnum.indexOf( randNum ) !== -1 ) return setSelected({...selected,num:"Number already selected."});
-        setSelected(prevState=>{
-          return {
-              num : randNum,
-              xnum : [...prevState.xnum,randNum],
-              rnum : prevState.rnum
-          }
-        }); 
-      },3000);
+      }
       
-  }
+      let newArr = arr.filter(x=>numrange.xnum.indexOf(x) === -1);
 
-  const GeneratingNum = ()=>{
-      return  <div className="loader-wrap"><div className="loader"></div><small><em>Loading...</em></small></div>;
+      if( newArr.length <= 0 ) return;
+
+      let selectedNum = newArr.length > 1 ? Math.floor(Math.random() * (Math.max(...newArr) - Math.min(...newArr)) + Math.min(...newArr)) : newArr.pop();
+
+      if( numrange.xnum.indexOf(selectedNum) === -1 ){          
+          setNumrange(prevState=>{
+              return {
+                  start : prevState.start,
+                  end : prevState.end,
+                  xnum : [...prevState.xnum,selectedNum]                
+              }
+          });    
+      }
+
+      console.log("Selected :",selectedNum,"Remaining : ",newArr);
+
   }
 
   return (
     <div className="App">
-        {selected.num ? <h1>{selected.num}</h1> : <GeneratingNum />}
+        
+        <h1>{selected}</h1>
         <small>
-          <em>Eliminated numbers : {selected.xnum.join(',')}</em>< br/>
+            <em>Eliminated numbers : {numrange.xnum.join(',')}</em>< br/>
         </small>
-        <input type="text" placeholder="from" name="min" onChange={e=>{setNumRange({...numrange,min : parseInt(e.currentTarget.value)})}}/>
-        <input type="text" placeholder="to" name="max" onChange={e=>{setNumRange({...numrange,max:parseInt(e.currentTarget.value)})}}/>
-        <button onClick={randomize}>Eliminate</button>
+        <input type="text" placeholder="Start" name="start" onChange={(e)=>{        
+            let startNum = parseInt(e.target.value);    
+            setNumrange(prevState=>{
+                return {
+                    ...prevState,
+                    start : parseInt(startNum),
+                }
+            });
+        }} />
+        <input type="text" placeholder="End" name="end" onChange={(e)=>{
+            let endNum = parseInt(e.target.value);
+            setNumrange(prevState=>{
+                return {
+                    ...prevState,
+                    end : parseInt(endNum)
+                }
+            });
+        }} />
+        <button onClick={handleNumber}>Eliminate</button>
     </div>
   );
 }
---
+++ src/component/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Loader() {
+    return (
+        <div className="loader-wrap">
+            <div className="loader"></div>
+            <small><em>Randomizing Number</em></small>
+        </div>
+    )
+}
+
+export default Loader
